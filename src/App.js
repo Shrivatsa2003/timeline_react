eaf1235fab831bf2b3fa068f4a9fe8387601f4e5
shrivatsa <shreevatsakc2003@gmail.com>
2023-11-26 00:42:50 +0530

    basic timeline

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+
+import './App.css';
+
+import timelineElements from './timelineElements';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+function App() {
+  let dotIconStyle = { background: '#06D6A0' };
+  return (
+     <div className='dotted-box'>
+      <h1 className='title'>Dates and Schedule</h1>
+      <VerticalTimeline>
+        {
+        timelineElements.map((element) => (
+          <VerticalTimelineElement
+            key={element.id}
+            date={element.description}
+            dateClassName="date"
+            iconStyle={dotIconStyle} 
+          >
+            <h3 className='vertical-timeline-element-title'>{element.title}</h3>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+      
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/timelineElements.js
@@ -0,0 +1,30 @@
+let timelineElements =[
+    {
+        id:1,
+        title:"Ideathon",
+        description:"1st to 2nd December"
+    },
+    {
+        id:2,
+        title:"Ideathon Results",
+        description:"4th december"
+    },
+    {
+        id:3,
+        title:"Mentorship duration",
+        description:"5th to 20th December"
+    },
+    {
+        id:3,
+        title:"Hackathon",
+        description:"22nd to 23rd December"
+    },
+    {
+        id:4,
+        title:"Hackathon Results",
+        description:"24th to 25th December"
+    }
+
+
+]
+export default timelineElements
